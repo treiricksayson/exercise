429a6f9dbc9e7f2e49f8d704bfcedfd05fd94d02
Eirick Sayson <eirick.sayson@tooltwist.com>
2022-09-07 14:11:06 +0800

    Update Code

--- src/main/java/com/solidprinciple/exercise/web/CalculateController.java
+++ src/main/java/com/solidprinciple/exercise/web/CalculateController.java
@@ -1,6 +1,5 @@
 package com.solidprinciple.exercise.web;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
